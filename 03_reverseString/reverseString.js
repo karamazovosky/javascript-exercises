fcaba74fa13cf11e348a6bcae73785ae111a0d57
karamazovosky <karamazovosky@gmail.com>
2022-01-22 19:33:10 +0800

    Do exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
 const reverseString = function(str) {
-
+//Turn str into object
+let word = str.split('');
+let reversed = '';
+let length = str.length-1;
+for (length; length >= 0; length--){
+    reversed = reversed.concat(word[length]);
+}
+return reversed;
 };
 
 // Do not edit below this line
