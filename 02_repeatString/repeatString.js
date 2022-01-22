352f44c1ffc50c62cf5d2c4e99213b75725229e0
karamazovosky <karamazovosky@gmail.com>
2022-01-22 19:22:39 +0800

    Do exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    //Set text variable
+    let text = '';
+    if (times >= 0){
+        for(let i = 0; i<times; i++) {
+            text += word;
+        }
+        return text;
+    }else return 'ERROR'
 };
 
 // Do not edit below this line
