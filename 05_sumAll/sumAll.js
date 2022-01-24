5776f678750861ee07166aec68339abf852be980
karamazovosky <karamazovosky@gmail.com>
2022-01-24 17:22:27 +0800

    Do exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(...args) {
+    //define start point
+    let input1 = args[0];
+    //define end point
+    let input2= args[1];
+    //define container for sumall
+    let sumall = 0;
+    let negativeCheck = (input1 >= 0) && (input2 >= 0);
+    let numberCheck = typeof input1 === 'number' && typeof input2 === 'number';
+    if (negativeCheck && numberCheck) {
+    //define bigger value
+        let start = (input1 > input2) ? input2 : input1
+        let end = (input1 < input2) ? input2: input1
+        //loop from start to end
+        for(let i = start; i <= end; i++) {
+            sumall += i;
+        }
+        return sumall;
+    }else return 'ERROR'
+   
 };
 
 // Do not edit below this line
