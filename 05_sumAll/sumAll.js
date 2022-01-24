4b98c79122fbd9515d53664296c881514eab30c6
karamazovosky <karamazovosky@gmail.com>
2022-01-24 17:41:26 +0800

    Better formatting

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -4,19 +4,22 @@ const sumAll = function(...args) {
     //define end point
     let input2= args[1];
     //define container for sumall
-    let sumall = 0;
+    let sumall = 'ERROR';
     let negativeCheck = (input1 >= 0) && (input2 >= 0);
     let numberCheck = typeof input1 === 'number' && typeof input2 === 'number';
     if (negativeCheck && numberCheck) {
-    //define bigger value
-        let start = (input1 > input2) ? input2 : input1
-        let end = (input1 < input2) ? input2: input1
+        sumall = 0;
+        //define bigger value
+        let start;
+        let end;
+        //using ternary, because if its non number, the Math.max and Math.min method return same value
+        start = (input1 > input2) ? input2 : input1;
+        end = (input1 < input2) ? input2: input1;
         //loop from start to end
         for(let i = start; i <= end; i++) {
             sumall += i;
         }
-        return sumall;
-    }else return 'ERROR'
+    }return sumall;
    
 };
 
