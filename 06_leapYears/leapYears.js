b4d5fb0d120842240184061c38f60a74d603eb57
karamazovosky <karamazovosky@gmail.com>
2022-01-24 19:02:03 +0800

    Compare to solution

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -12,7 +12,10 @@ const leapYears = function(year) {
         
     }else status = false;
     return status;
+    // return year % 4 === 0 && (year % 100 !== 0 || year % 400 ===0);
+
 };
+
 
 // Do not edit below this line
 module.exports = leapYears;
