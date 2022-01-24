09624898c4092c3e8d1ca1c4472f9d8bb61a8d8c
karamazovosky <karamazovosky@gmail.com>
2022-01-25 01:01:49 +0800

    Do exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if(number<0) {
+        return "OOPS"
+    }
+    let start = 0;
+    let first = 1;
+    let container = 0;
+    for(let i=0; i<number; i++)  {
+        container = start + first; //1 2 3 5
+        start = first; //1 1 2 3
+        first = container; //1 2 3 5
+    }
+    return start
 };
 
 // Do not edit below this line
