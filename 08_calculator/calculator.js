b4f9f01345b5701e525517b220e9328612bb4e7b
karamazovosky <karamazovosky@gmail.com>
2022-01-24 19:45:54 +0800

    Do exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let summation = 0;
+  for (let i = 0; i<array.length; i++) {
+    summation += array[i];
+  }
+  return summation;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let summation = 1;
+  for (let i = 0; i<array.length; i++) {
+    summation *= array[i];
+  }
+  return summation;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	let summation = 1;
+  for (let i = 1; i<number+1; i++) {
+    summation *= i;
+  }
+  return summation;
 };
 
 // Do not edit below this line
