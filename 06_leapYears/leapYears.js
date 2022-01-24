41f95b4803037eae0d09fd6527d5c5118e8bc45e
karamazovosky <karamazovosky@gmail.com>
2022-01-24 18:58:24 +0800

    Do exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let status = false;
+    //check if divisible by 4 first
+    if (year%4 === 0) {
+        status = true;
+        if(year%100 === 0) {
+            status = false
+            if(year%400 === 0) {
+                status = true
+            }
+        }
+        
+    }else status = false;
+    return status;
 };
 
 // Do not edit below this line
