00f33673de25880c9d6a4af9eda9fc708aaf9e14
karamazovosky <karamazovosky@gmail.com>
2022-04-28 17:54:49 +0800

    Do exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(obj) {
+  let temp = 0;
+  for (let k in obj) {
+    if (typeof obj[k] === "number"){
+      temp += obj[k];
+    }
+  }
+  return temp;
 };
 
-const multiply = function() {
-
+const multiply = function(obj) {
+  let temp = 1;
+  for (let k in obj) {
+    if(typeof obj[k] === "number") {
+      temp *= obj[k];
+    }
+  }
+  return temp;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let temp = 1;
+	for (let i=1; i<=a; i++) {
+    temp *= i;
+  }
+  return temp;
 };
 
 // Do not edit below this line
