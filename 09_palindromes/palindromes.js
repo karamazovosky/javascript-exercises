d6338f6917ea2f4367a88efd70f3035dd32a1c1a
karamazovosky <karamazovosky@gmail.com>
2022-04-29 18:38:07 +0800

    Do exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    let splitString = input.toLowerCase().split("");
+    let cleanString = [];
+    for (let i = 0; i<splitString.length; i++) {
+        if(splitString[i].match(/^[.,:!?^ ]/)){
+            continue
+        }
+        cleanString.push(splitString[i]);
+    }
+    //reversed string
+    let reversedString = [];
+    let cleanstrL = cleanString.length;
+    for(let i = cleanstrL-1; i>=0; i--) {
+        reversedString.push(cleanString[i]);
+    }
+    let endcleanString = cleanString.join('');
+    let endreversedString = reversedString.join('');
+    if(endcleanString==endreversedString) {
+        return true
+    }else return false
 };
 
 // Do not edit below this line
