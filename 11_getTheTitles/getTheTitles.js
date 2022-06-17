af0250c7bd7642ce1092e93c58072653744ac14f
karamazovosky <karamazovosky@gmail.com>
2022-06-17 14:14:03 +0800

    Do exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(input) {
+    //define the database length
+    let dataLength = input.length;
+    let temp =[];
+    for (let i=0; i<dataLength; i++){
+        temp.push(input[i].title)
+    }
+    return temp;
 };
 
 // Do not edit below this line
