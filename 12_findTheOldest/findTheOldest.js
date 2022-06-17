bff39a0f59ef799284fd3a247866d59267a85843
karamazovosky <karamazovosky@gmail.com>
2022-06-17 15:05:59 +0800

    Do exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(peopleList) {
+    let peopleListLength = peopleList.length;
+    //define new array
+    let processedpeopleList = [];
+    //loop to input people's age into the array
+    for (let i=0; i<peopleListLength; i++){
+        if (peopleList[i].yearOfDeath == undefined) {
+            let temp = new Date().getFullYear();
+            peopleList[i].yearOfDeath = temp;
+        }
+        processedpeopleList.push(peopleList[i].yearOfDeath-peopleList[i].yearOfBirth)
+    }
+    //function to find the max value in the array and store it as variable
+    let oldestAge = Math.max(...processedpeopleList);
+    let arrCount = 0;
+    for (let i=0; i<processedpeopleList.length; i++){
+        if(processedpeopleList[i] == oldestAge) {
+            break
+        }
+        arrCount++
+    }
+    // return the oldest person
+    return peopleList[arrCount];
 };
 
 // Do not edit below this line
